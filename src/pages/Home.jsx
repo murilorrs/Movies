034b344987317342230a1ffdbdbfda3911c21895
Murilo <murilorodriguesrrs@gmail.com>
2024-06-27 08:13:18 -0300

    update Melhores avaliacoes / Populares

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,32 +8,71 @@ const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
   const [latestMovies, setLatestMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([])
+  const [BestRatedMovies, setBestRatedMovies] = useState([])
 
   const getLatestdMovies = async (url) =>{
     const res = await fetch(url)
     const data = await res.json()
 
     setLatestMovies(data.results)
+    // console.log(data.results)
+  }
+
+   const getPopularMovies = async (url) =>{
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setPopularMovies(data.results)
+  }
+  
+  const getBestRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    
+    setBestRatedMovies(data.results)
     console.log(data.results)
   }
 
   useEffect(() => {
     const latestUrl = `${moviesURL}now_playing?${apiKey}`
+    const popularUrl = `${moviesURL}popular?${apiKey}`
+    const bestRatedUrl = `${moviesURL}top_rated?${apiKey}`
 
     getLatestdMovies(latestUrl)
+    getPopularMovies(popularUrl)
+    getBestRatedMovies(bestRatedUrl)
 
   }, []);
 
   return (
-  <div className ="container">
-    <h2 className="title">Novos Lançamentos!</h2>
-    {/* <p>Os ultimos lançamentos do cinema!</p> */}
-    <div className="movies-container">
-      {latestMovies.length === 0 && <p>Carregando...</p>}
-      {latestMovies.length > 0 && 
-      latestMovies.map(movie => <MovieCard key={movie.id} movie ={movie} />)}
+  <>
+
+    
+    <div className ="container">
+      <h2 className="title">Novos lançamentos<span>!</span></h2>
+      <div className="movies-container">
+        {latestMovies.length === 0 && <p>Carregando...</p>}
+        {latestMovies.length > 0 && 
+        latestMovies.map(movie => <MovieCard key={movie.id} movie ={movie} />)}
+      </div>
+
+    <h2 className="title">Os mais populares<span>!</span></h2>
+    <div className="movies-container populares">
+        {popularMovies.length === 0 && <p>Carregando...</p>}
+        {popularMovies.length > 0 &&
+        popularMovies.map(movie => <MovieCard key={movie.id} movie={movie}/>)}
     </div>
+
+    <h2 className="title">Filmes aclamados pela crítica<span>!</span></h2>
+    <div className="movies-container populares">
+        {BestRatedMovies.length === 0 && <p>Carregando...</p>}
+        {BestRatedMovies.length > 0 &&
+        BestRatedMovies.map(movie => <MovieCard key={movie.id} movie={movie}/>)}
+    </div>
+
   </div>
+  </>
   )
 }
 
